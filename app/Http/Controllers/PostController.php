69de7f4d3112d910dbbf4e36487ae21ed282c8cf
rkalfonso <hhmps17@gmail.com>
2021-01-25 09:24:25 +0800

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        Post::create([
+            'user_id' => auth()->id(),
+            'body' => $request->body
+        ]);
+    }
+}
